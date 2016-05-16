8eeb54ee553b98a807118b73966514e57b3504a8
Ian Sweet <iannsweet@gmail.com>
2016-05-16 14:55:06 -0400

    z3 package requires specific version of z3 include/ and lib/

--- try.sh
+++ try.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cabal sandbox init
-cabal install --only-dependencies --enable-tests --extra-include-dirs=/usr/local/include --extra-lib-dirs=/usr/local/lib
+cabal install --only-dependencies --enable-tests --extra-include-dirs=/usr/local/Cellar/z3/4.4.1/include --extra-lib-dirs=/usr/local/Cellar/z3/4.4.1/lib
 cabal configure --enable-tests
 cabal build
 cabal test --show-details=streaming
