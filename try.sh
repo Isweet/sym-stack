ca76d82ab6ed8b8b3c093c0c599fbb2b3e9d64f9
Ian Sweet <iannsweet@gmail.com>
2016-05-24 11:35:42 -0400

    cabal streaming test messes up IO, just run binary

--- try.sh
+++ try.sh
@@ -4,5 +4,4 @@ cabal sandbox init
 cabal install --only-dependencies --enable-tests --extra-include-dirs=/usr/local/Cellar/z3/4.4.1/include --extra-lib-dirs=/usr/local/Cellar/z3/4.4.1/lib
 cabal configure --enable-tests
 cabal build
-cabal test --show-details=streaming
-
+./dist/build/symstack-test/symstack-test
